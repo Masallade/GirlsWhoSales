3a7d54079ad1a39a03ba2efe40709e85bc575ee0
asad2401 <asadmalik2401@gmail.com>
2022-06-07 14:49:17 +0500

    "New Chnages After All updation"

---
+++ lib/model/profileUpdate.dart
@@ -0,0 +1,18 @@
+class ProfileUpdate {
+  String message;
+  String status;
+
+  ProfileUpdate({this.message, this.status});
+
+  ProfileUpdate.fromJson(Map<String, dynamic> json) {
+    message = json['message'];
+    status = json['status'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['message'] = this.message;
+    data['status'] = this.status;
+    return data;
+  }
+}
